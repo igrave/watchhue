eb9753258fc47846499fd944cb9a84ea7243854b
igrave <isaac.gravestock@uzh.ch>
2019-03-12 21:55:23 +0100

    functions for local connections

--- local.py
+++ local.py
@@ -1,21 +1,26 @@
 import requests
 
 my_ip =  '192.168.0.45'
-my_username = 
+my_username = 'pPhwa5anSiPhiJ05YU4fNUENz-RTrcCoZututZwz'
 
-sens = requests.get('http://'+my_ip+'/api/'+my_username+'/sensors') 
-sj = sens.json()
-sens.ids = sj.keys()
-pres_ids = []
+def get_sensor_state(my_ip, my_username, pres_ids):
+    pres_states = []
+    for id in pres_ids:
+        r = requests.get('http://'+my_ip+'/api/'+my_username+'/sensors/'+id) 
+        pres_states.append(r.json()["state"])
+    
+    return pres_states
 
-for key in sens.ids:
-    if sj[key]['type'] == 'ZLLPresence':
-        sj[key]['name']
-        pres_ids.append(key)
+def get_pres_sensor_states(my_ip, my_username):
+    sens = requests.get('http://'+my_ip+'/api/'+my_username+'/sensors') 
+    sj = sens.json()
+    sens_ids = sj.keys()
+    pres_ids = []
+    for key in sens_ids:
+        if sj[key]['type'] == 'ZLLPresence':
+            sj[key]['name']
+            pres_ids.append(key)
+    return pres_ids
 
-print(pres_ids)
 
-for id in pres_ids:
-    r = requests.get('http://'+my_ip+'/api/'+my_username+'/sensors/'+id) 
-    print(r.json()["state"])
-
+    
